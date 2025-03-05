c2b646c1b4df50b8a30c563295545fee3dbe2adc
soniya3AE <soniyakamble27@gmail.com>
2025-03-06 00:23:14 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DevopsSampleProjctApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsSampleProjctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSampleProjctApplication.class, args);
+	}
+
+}
